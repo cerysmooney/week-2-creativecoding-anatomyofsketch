b67ef0f9a12b6c182c6896ca54333585f37a0b3a
cerys <cerys2.mooney@live.uwe.ac.uk>
2021-10-21 12:04:23 +0100

    added another rectangle

--- p5 (1)/empty-example/sketch.js
+++ p5 (1)/empty-example/sketch.js
@@ -1,5 +1,5 @@
-let rectWidth = 45;
-let rectHeight = 90;
+let rectWidth = 25;
+let rectHeight = 25;
 function setup() {
   createCanvas(800,600);
   background(255);
@@ -12,7 +12,8 @@ function draw() {
   line(0,50,400,70);
   triangle(347,54,392,9,392,66);
   quad(158,55,199,14,392,66,351,197);
-  rect(60,80,rectWidth,rectHeight);
+  rect(25,50,rectWidth,rectHeight);
+  rect(525,67,rectWidth,rectHeight);
   arc(300,300,50,50,90,270);
   // put drawing code here
 }
