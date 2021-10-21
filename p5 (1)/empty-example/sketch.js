933e3a439db4782fe85e9acbe96c71125ed0f62d
cerys <cerys2.mooney@live.uwe.ac.uk>
2021-10-21 11:55:39 +0100

    added shapes and text

--- p5 (1)/empty-example/sketch.js
+++ p5 (1)/empty-example/sketch.js
@@ -1,10 +1,16 @@
 function setup() {
   createCanvas(800,600);
-  background(0);
+  background(255);
   // put setup code here
 }
 
 function draw() {
-  
+  text("Hello World!",0,50);
+  ellipse(50,50,40,40);
+  line(0,50,400,70);
+  triangle(347,54,392,9,392,66);
+  quad(158,55,199,14,392,66,351,197);
+  rect(25,50,25,25);
+  arc(300,300,50,50,90,270);
   // put drawing code here
 }
