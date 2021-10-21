4804f93df7dce805e36a9cbab190a21f6e1263a3
cerys <cerys2.mooney@live.uwe.ac.uk>
2021-10-21 12:00:10 +0100

    played with rectangle size

--- p5 (1)/empty-example/sketch.js
+++ p5 (1)/empty-example/sketch.js
@@ -1,3 +1,5 @@
+let rectWidth = 45;
+let rectHeight = 90;
 function setup() {
   createCanvas(800,600);
   background(255);
@@ -10,7 +12,7 @@ function draw() {
   line(0,50,400,70);
   triangle(347,54,392,9,392,66);
   quad(158,55,199,14,392,66,351,197);
-  rect(25,50,25,25);
+  rect(60,80,rectWidth,rectHeight);
   arc(300,300,50,50,90,270);
   // put drawing code here
 }
